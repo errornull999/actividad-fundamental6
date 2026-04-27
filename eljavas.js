7269c3a08cbe13ee1a5031f313deb2900871de26
Arturo Alejandro Tamez Mendez <arthurtm06@gmail.com>
2026-04-26 22:07:23 -0600

    Modificaciones realizadas por mclovin (Arturo Alejandro Tamez Mendez)

--- eljavas.js
+++ eljavas.js
@@ -4,7 +4,8 @@ function mostrarmemealeatorio() {
         "https://images7.memedroid.com/images/UPLOADED606/60688f3f3250e.jpeg",
         "https://heraldodemexico.com.mx/wp-content/uploads/2019/09/java_programador-932x1024.jpg",
         "https://images7.memedroid.com/images/UPLOADED763/650a995496bd2.jpeg",
-        "https://images3.memedroid.com/images/UPLOADED175/65bbbf701ceed.jpeg"
+        "https://images3.memedroid.com/images/UPLOADED175/65bbbf701ceed.jpeg",
+        "https://i.ibb.co/mCwPL7DF/codigo.jpg",
     ];
     let memealeatorio = memes[Math.floor(Math.random() * memes.length)];
     document.querySelector("#meme").innerHTML = `<img src="${memealeatorio}" alt="Meme Aleatorio">`;
